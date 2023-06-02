c62d2327821d51d9805fb2f686971fb72b560820
Yamadharma963 <134951174+Yamadharma963@users.noreply.github.com>
2023-06-02 14:52:32 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        color1: "#f59e0b",
+        color2: "#57534e",
+        color3: "#fafaf9",
+        color4: "#0a0a0a",
+        color5: "#3b82f6",
+      },
+    },
   },
   plugins: [],
 };
